df6a12bad457b0b605ac83f27e900ea17c17a76d
Grigoriy <grishkoshkarta1@gmail.com>
2023-04-13 15:50:47 +0300

    Block4 working on sliider

--- src/sections/block4/Block4.tsx
+++ src/sections/block4/Block4.tsx
@@ -1,3 +1,8 @@
+import {useEffect, useState} from "react";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 import graphQl from "../../assets/slides/graphQL.svg";
 import mobx from "../../assets/slides/mobx.svg";
 import mongo from "../../assets/slides/mongodb.svg";
@@ -15,13 +20,28 @@ import webpack from "../../assets/slides/webpack.svg";
 import s from "./Block4.module.scss";
 
 const Block4 = () => {
+    const [currentIco, setCurrentIco] = useState(8);
+
     const imgArr = [graphQl, mobx, mongo, nestJS, nodeJS, postgre, react, recoil, redux, reduxsaga, three, vue, webpack];
     const imgArrExtended = [...imgArr, ...imgArr, ...imgArr];
+
+    const currentIcoName = imgArrExtended.find((_, idx) => idx === currentIco)?.match(/\/media\/(.+?)\..+/)?.[1];
+    const icoName = currentIcoName ? currentIcoName.toUpperCase() : "";
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentIco((currIco) =>
+                currentIco + 1 >= imgArrExtended.length ? 0 : currIco + 1
+            );
+        }, 3150);
+        return () => clearInterval(intervalId);
+    }, [currentIco, imgArrExtended.length]);
 
     return (
         <div className={s.block}>
             <h1 className={s.title}>TECHNOLOGY</h1>
             <div className={s.table}>
+                <h2 className={s.nameIcon}>{icoName}</h2>
                 <div className={s.sliderTrack}>
                     {imgArrExtended.map((img, idx) => (
                         <div className={s.slide} key={idx}>
