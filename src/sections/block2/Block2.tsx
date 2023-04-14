51f932a1d3683f28728254df824feec41b6e1478
kstroevsky <ks7.1498@gmail.com>
2023-04-14 14:27:10 +0300

    refactor/blocks-sizes

--- src/sections/block2/Block2.tsx
+++ src/sections/block2/Block2.tsx
@@ -9,18 +9,22 @@ function Block2() {
 					<div className={s.top}></div>
 				</div>
 				<div>
-					<div className={s.titleList}>
-						<h2 className={`${s.titleList__item} ${s.titleList__item_first}`}>
-							SPEED UP DEVELOPMENT
-						</h2>
-						<h2 className={`${s.titleList__item} ${s.titleList__item_second}`}>
-							INCREASE QUALITY
-						</h2>
-						<h2 className={`${s.titleList__item} ${s.titleList__item_third}`}>
-							REDUCE COSTS
-						</h2>
+					<div>
+						<div className={s.titleList}>
+							<h2 className={`${s.titleList__item} ${s.titleList__item_first}`}>
+								SPEED UP DEVELOPMENT
+							</h2>
+							<h2
+								className={`${s.titleList__item} ${s.titleList__item_second}`}
+							>
+								INCREASE QUALITY
+							</h2>
+							<h2 className={`${s.titleList__item} ${s.titleList__item_third}`}>
+								REDUCE COSTS
+							</h2>
+						</div>
+						<h1 className={s.titleBottom}>WHAT WE DO</h1>
 					</div>
-					<h1 className={s.titleBottom}>WHAT WE DO</h1>
 					<div className={s.bottom}></div>
 				</div>
 			</div>
--- src/sections/block3/Block3.tsx
+++ src/sections/block3/Block3.tsx
@@ -1,62 +1,20 @@
-import first from '../../assets/block3/first.png';
-import second from '../../assets/block3/second.png';
-import third from '../../assets/block3/third.png';
+import classnames from 'classnames';
 
 import s from './Block3.module.scss';
-import { useState } from 'react';
 
 const Block3 = () => {
-	const [backgroundImage, setBackgroundImage] = useState('');
-
-	const handleMouseEnter = (image: string) => {
-		setBackgroundImage(image);
-	};
-
-	const handleMouseLeave = () => {
-		setBackgroundImage('');
-	};
-
 	return (
-		// <div className={s.block}>
-		//     <div className={`${s.section} ${s.section_first}`}>
-		//         <img className={s.section__right} src={first} alt="img"/>
-		//     </div>
-		//     <div className={`${s.section} ${s.section_second}`}>
-		//         <img className={s.section__left} src={second} alt="img"/>
-		//     </div>
-		//     <div className={`${s.section} ${s.section_third}`}>
-		//         <img className={s.section__right} src={third} alt="img"/>
-		//     </div>
-		// </div>
 		<div className={s.block}>
-			<div
-				className={s.background}
-				style={{
-					transition: 'backgroundImage 0.4s ease',
-					backgroundImage: `url(${backgroundImage})`,
-				}}
-			></div>
-			<div
-				className={`${s.section} ${s.section_first}`}
-				onMouseEnter={() => handleMouseEnter(first)}
-				onMouseLeave={handleMouseLeave}
-			>
-				<img className={s.section__right} src={first} alt="img" />
+			<div className={classnames(s.section, s.section_first)}>
+				<div className={s.section__right}></div>
 			</div>
-			<div
-				className={`${s.section} ${s.section_second}`}
-				onMouseEnter={() => handleMouseEnter(second)}
-				onMouseLeave={handleMouseLeave}
-			>
-				<img className={s.section__left} src={second} alt="img" />
+			<div className={classnames(s.section, s.section_second)}>
+				<div className={s.section__left}></div>
 			</div>
-			<div
-				className={`${s.section} ${s.section_third}`}
-				onMouseEnter={() => handleMouseEnter(third)}
-				onMouseLeave={handleMouseLeave}
-			>
-				<img className={s.section__right} src={third} alt="img" />
+			<div className={classnames(s.section, s.section_third)}>
+				<div className={s.section__right}></div>
 			</div>
+			<div className={classnames(s.background)}></div>
 		</div>
 	);
 };
