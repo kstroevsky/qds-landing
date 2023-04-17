d7d467dddfb3ea3bf572f9c4c91966047454cb85
kstroevsky <ks7.1498@gmail.com>
2023-04-17 11:54:43 +0300

    refactor/heights

--- src/sections/formBlock/components/userChoice/UserChoice.tsx
+++ src/sections/formBlock/components/userChoice/UserChoice.tsx
@@ -1,17 +1,14 @@
-import UserChoiceButtons from "./UserChoiceButtons"
+import UserChoiceButtons from './UserChoiceButtons';
+import { formOptions } from '../../../../shared/constants';
 
-import s from "./UserChoice.module.scss";
+import s from './UserChoice.module.scss';
 
-const UserChoice = () => {
-    const items = ['UI/UX design', 'Frontend', 'Graphic design', 'Devops', 'Architecture']
-
-    return (
-        <div className={s.userChoice}>
-            {items.map(el =>
-                <UserChoiceButtons key={el} title={el}/>
-            )}
-        </div>
-    );
-};
+const UserChoice = () => (
+	<div className={s.userChoice}>
+		{formOptions.map((el) => (
+			<UserChoiceButtons key={el} title={el} />
+		))}
+	</div>
+);
 
 export default UserChoice;
---
+++ src/shared/constants/index.ts
@@ -0,0 +1,7 @@
+export const formOptions = [
+	'UI/UX design',
+	'Frontend',
+	'Graphic design',
+	'Devops',
+	'Architecture',
+];
