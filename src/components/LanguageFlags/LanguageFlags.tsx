a33b91ab1e4a85e8dd16fb6d79ca6f83b811a46f
Grigoriy <grishkoshkarta1@gmail.com>
2023-04-27 19:15:22 +0300

    style/animation change flag

--- src/components/LanguageFlags/LanguageFlags.tsx
+++ src/components/LanguageFlags/LanguageFlags.tsx
@@ -4,25 +4,27 @@ import type { FC } from 'react';
 import i18next from 'i18next';
 import { ELanguages } from '../../shared/constants';
 
+import ua from "../../assets/flags/ukraine.png";
+import en from "../../assets/flags/england.png";
+
 import s from './LanguageFlags.module.scss';
 
 const LanguageFlags: FC = () => {
 	const [isUA, setIsUA] = useState<boolean>(true);
+	const [activeFlag, setActiveFlag] = useState(i18next.language)
+
+	console.log(activeFlag)
 
 	const handleChangeLanguage = useCallback((lang: ELanguages) => {
 		setIsUA((prev) => !prev);
 		i18next.changeLanguage(lang);
+		setActiveFlag(i18next.language)
 	}, []);
 
 	return (
-		<div className={s.wrapper}>
-			<h1
-				onClick={() => {
-					handleChangeLanguage(isUA ? ELanguages.UA : ELanguages.EN);
-				}}
-			>
-				{isUA ? ELanguages.UA : ELanguages.EN}
-			</h1>
+		<div className={s.wrapper} onClick={() => {handleChangeLanguage(isUA ? ELanguages.UA : ELanguages.EN);}}>
+			<img className={s.flags} src={activeFlag === 'ua' ? ua : en} alt={'ua'} />
+			<img className={s.flags} src={activeFlag !== 'en' ? en : ua} alt={'en'} />
 		</div>
 	);
 };
